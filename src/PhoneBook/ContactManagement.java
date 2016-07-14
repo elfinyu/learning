25892dd41ae9cba2986b89a0b98af0015e04b942
yudan <yudan@1bank.dbs.com>
2016-07-14 22:38:42 +0800

    ContactManagement AlphabeticalComparator

--- src/PhoneBook/ContactManagement.java
+++ src/PhoneBook/ContactManagement.java
@@ -2,17 +2,22 @@ package PhoneBook;
 
 import java.io.PrintStream;
 import java.lang.reflect.Field;
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+
+//import blur.BlurContactManagement.AlphabeticalComparator;
 
 public class ContactManagement implements PhoneBook {
 
 	final protected Collection<Contact> contactList;
 	final HashMap<String,Collection<Contact>> contactGroupsHashMap;
+	public static final Comparator CONTACT_NAME_COMPARATOR_ALPHABETICAL = new AlphabeticalComparator();
 	public ContactManagement(){
 		this.contactList = new ArrayList<Contact>();
 		this.contactGroupsHashMap = new HashMap<String,Collection<Contact>>();
@@ -107,24 +112,34 @@ public class ContactManagement implements PhoneBook {
 
 	@Override
 	public Collection<Contact> sortContactByName() {
-		Collection<Contact> sortedList=new ArrayList<Contact>();
-		
-		List<String> array=new ArrayList<String>();
-		for(Contact contact:contactList){
-			array.add(contact.toString());
-		}	
-		//Collections.sort(array, String.CASE_INSENSITIVE_ORDER);
-		Collections.sort(array);
-		System.out.println();
-		for(String x:array){
-			for(Contact contact:contactList){
-				if(contact.toString().equals(x)){
-					sortedList.add(contact);
-				}
-			}
-		}
+
+		List<Contact> sortedList=(List)this.contactList ;
+		System.out.println(sortedList);
+		Collections.sort(sortedList, CONTACT_NAME_COMPARATOR_ALPHABETICAL);
 		return sortedList;
 //		Class<Contact> clazz=Contact.class;
 //		Field[] fields=clazz.getDeclaredFields();
 	}
+	
+	private static class AlphabeticalComparator implements Comparator<Contact> {
+
+		@Override
+		public int compare(Contact o1, Contact o2) {
+			// TODO Auto-generated method stub
+			return compare(o1.getName(),o2.getName());
+		}
+		
+		public int compare(String source, String target) {
+			// TODO Auto-generated method stub
+			 Collator myCollator = Collator.getInstance();
+			 if( myCollator.compare(source,target) < 0 ){
+				 return -1;
+			 }else if( myCollator.compare(source,target) == 0 ){
+				 return 0;
+			 }else{
+				 return 1;
+			 }
+		 }
+
+	}	
 }
