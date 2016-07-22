f9c69ec3812f8201012a316dad30773735c57fea
yudan <yudan@1bank.dbs.com>
2016-07-22 15:12:03 +0800

    added deleted files

---
+++ WebTesting/src/main/java/page/search/NameResultsPanel.java
@@ -0,0 +1,42 @@
+package page.search;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import page.PageObject;
+
+public class NameResultsPanel extends PageObject {
+
+	public NameResultsPanel(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/** 
+	 * Define locators for parent
+	*/
+	@FindBy(xpath="//a[@name='nm']/ancestor::div[@class='findSection']")
+	WebElement nameResultsDiv;
+	
+	/**
+	 * Define locators for child objects for results links
+	 */
+	By results_text_locator = By.xpath(".//td[@class='result_text']");
+	By results_text_locator_links = By.xpath(".//td[@class='result_text']/a");
+	
+	public int getNameResultsCount(){
+		return this.getElementCount(nameResultsDiv, results_text_locator);
+	}
+	
+	public int getNameResultsLinksCount(){
+		return this.getElementCount(nameResultsDiv, results_text_locator_links);	
+	}
+	
+	public void clickNameLink(String strLinkLabel){
+		this.clickOnLinkOfEqualString(nameResultsDiv, results_text_locator, strLinkLabel);
+	}
+}
---
+++ WebTesting/src/main/java/page/search/ResultPage.java
@@ -0,0 +1,33 @@
+package page.search;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import page.PageObject;
+
+public class ResultPage extends PageObject {
+
+	public TitleResultsPanel titleResults;
+	public NameResultsPanel nameResults;
+
+	public ResultPage(WebDriver driver) {
+		super(driver);
+		this.titleResults = new TitleResultsPanel(driver);
+		PageFactory.initElements(driver, titleResults);
+		this.nameResults = new NameResultsPanel(driver);
+		PageFactory.initElements(driver, nameResults);
+	}
+
+	public int getTitleResultsCount(String search) {
+		return titleResults.getTitleResultsCount(search);
+	}
+
+	public int getTitleResultsCountOfLinks(String search) {
+		return titleResults.getTitleResultsCountOfLinks(search);
+	}
+
+	public int getTitleResultsCountOfLinksUsingXpath(String search) {
+		return titleResults.getTitleResultsCountOfLinksUsingXpath(search);
+	}
+	
+}
