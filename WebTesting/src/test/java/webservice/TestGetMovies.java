086800f40d0a0b4934007c46ef3a316f2a7db9e5
yudan <yudan@1bank.dbs.com>
2016-07-31 13:03:11 +0800

    add webservice/TestGetMovies.java..

--- WebTesting/src/test/java/webservice/TestGetMovies.java
+++ WebTesting/src/test/java/webservice/TestGetMovies.java
@@ -8,8 +8,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.ExtractableResponse;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.AuthenticationSpecification;
+import com.jayway.restassured.specification.RequestSpecification;
+
+import data.Movie;
 
 public class TestGetMovies {
 
@@ -17,6 +30,93 @@ public class TestGetMovies {
 	public void getMovieByID() {
 		String asString = given().auth().basic("user1", "secret1").when().get("movie/1").asString();
 		System.out.println(asString);
+	}
+	
+	@Test
+	public void getMovieByIDRestAssured(){
+		/* 			Test step   |    Expected Result
+		 * 1.      Login using	|	 User should login successful
+		 * 2.      navigation to trade page | Trade page should appear 
+		 */
+		
+		
+		/* BDD behavior driven development
+		 * 
+		 * Given < precondition> >
+		 * When < action>
+		 * Then < expected result>
+		*/
+		
+		ExtractableResponse<Response> extract = RestAssured.
+		given()
+			.authentication().basic("yudan", "cute")
+		.when()
+			.get("movie/1")
+		.then()
+			.log().body()	
+			.statusCode(200)
+			.body("name", Matchers.equalTo("Hulk"))
+			.body("id",Matchers.equalTo(1))
+			.body("releaseCountry", Matchers.equalTo("USA"))
+			.body("duration_mins",Matchers.equalTo(138))
+			.body("releaseDate",Matchers.equalTo("20/06/2003"))
+			.contentType(ContentType.JSON)
+			.statusLine(Matchers.equalToIgnoringCase("HTTP/1.1 200 OK"))
+		.extract()
+			;
+		
+		Movie movie = extract.as(Movie.class);
+		System.out.println(movie.getName());
+		
+	}
+	
+	@Test
+	public void testGetAllMovies(){
+		/* BDD behavior driven development
+		 * 
+		 * Given < precondition> >
+		 * When < action>
+		 * And <action 2>
+		 * Then < expected result>
+		 * And <expect result 2>
+		 * 
+		 * Given < precondition> >
+		 * When < action>
+		 * When <action 2>
+		 * Then < expected result>
+		 * Then <expect result 2>
+		 *  
+		*/
+		ExtractableResponse<Response> extract = RestAssured.
+		given()
+			.authentication().basic("user1", "secret1")
+		.when()
+			.get("movie/all")
+		.then()
+			.log().body()
+			.statusCode(200)
+			.body("[0].name",Matchers.equalTo("Hulk"))
+			.body("[0].duration_mins", Matchers.equalTo(138))
+			.body("[0].id", Matchers.equalTo(1))
+			.body("[0].releaseCountry",Matchers.equalTo("USA"))
+			.body("[0].releaseDate", Matchers.equalTo("20/06/2003"))
+			.contentType(ContentType.JSON)
+			.body("list.size()", Matchers.greaterThanOrEqualTo(0))
+			.body("list.size()", Matchers.lessThan(10))
+			.extract()
+			;
+		
+		List<Movie> as = Arrays.asList( extract.as(Movie[].class));
+		for(Movie m:as){
+			System.out.println(m.getName());
+		}
+		
+//		RequestSpecification given2 = given();
+//		AuthenticationSpecification authentication = given2.authentication();
+//		RequestSpecification basic = authentication.basic("user", "password");
+//		basic.when().
+	
+		
 	}
 
 	@Test
